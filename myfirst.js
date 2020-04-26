de763b52c16205b24d436448a22c275813c69f2a
Carlos Diaz <cdiaz86@gmail.com>
2020-04-26 12:24:55 -0400

    Includes myDateModule.js.

--- myfirst.js
+++ myfirst.js
@@ -1,8 +1,11 @@
+// line 2 includes the http module through the use of the require function
 const http = require("http");
+const dt = require("./myDateModule");
 
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello World!");
+    res.write("The date and time are currently: " + dt.myDateTime());
+    res.end();
   })
   .listen(8080);
