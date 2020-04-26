58241870b919507db4f605d7fb5c3a869edcb9ff
Carlos Diaz <cdiaz86@gmail.com>
2020-04-26 13:46:03 -0400

    Imports url module to split query string.

--- myfirst.js
+++ myfirst.js
@@ -1,11 +1,21 @@
 // line 2 includes the http module through the use of the require function
 const http = require("http");
-const dt = require("./myDateModule");
+// const dt = require("./myDateModule");
+const url = require("url");
 
 http
+  // create server with .createServer()
   .createServer(function (req, res) {
+    // add content type with writeHead()
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("The date and time are currently: " + dt.myDateTime());
-    res.end();
+    // Shows info from imported myDateModule
+    // res.write("The date and time are currently: " + dt.myDateTime());
+    // URL property of the req argument passes text after / in URL
+    // res.write(req.url);
+
+    // splits query string
+    const q = url.parse(req.url, true).query;
+    const txt = q.year + " " + q.month;
+    res.end(txt);
   })
   .listen(8080);
